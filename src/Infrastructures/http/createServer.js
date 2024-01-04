20326934df27c81eb28ea8b170f6a8831f97689c
rplpadaherang <74759857+rplpadaherang@users.noreply.github.com>
2024-01-04 08:45:27 +0700

    Update createServer.js
    Fix conflict

--- src/Infrastructures/http/createServer.js
+++ src/Infrastructures/http/createServer.js
@@ -56,14 +56,6 @@ const createServer = async (container) => {
     },
   ]);
 
-  server.route({
-    method: 'GET',
-    path: '/',
-    handler: () => ({
-      value: 'Hello World!',
-    }),
-  });
-
   server.ext('onPreResponse', (request, h) => {
     // mendapatkan konteks response dari request
     const { response } = request;
