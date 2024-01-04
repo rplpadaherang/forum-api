b6a816aa7f4a3d2a5f7d00dc146e722a4ffb7915
rplpadaherang <74759857+rplpadaherang@users.noreply.github.com>
2024-01-04 08:44:28 +0700

    Update createServer.test.js
    Fix conflict

--- src/Infrastructures/http/_test/createServer.test.js
+++ src/Infrastructures/http/_test/createServer.test.js
@@ -15,23 +15,6 @@ describe('HTTP server', () => {
     expect(response.statusCode).toEqual(404);
   });
 
- describe('when GET /', () => {
-   it('should return welcome message', async () => {
-      // Arrange
-     const server = await createServer({});
-      // Action
-      const response = await server.inject({
-        method: 'GET',
-        url: '/',
-      });
-      // Assert
-      const responseJson = JSON.parse(response.payload);
-      expect(response.statusCode).toEqual(200);
-      expect(responseJson.value).toEqual('Hello World!');
-     });
-   });
-
-
   it('should handle server error correctly', async () => {
     // Arrange
     const requestPayload = {
